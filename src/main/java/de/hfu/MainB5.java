a75d830c2167741a49226c56ce226b61db5a12de
LebMax <d25max09@gmail.com>
2022-12-08 10:52:11 +0100

    added JavaDoc comments

--- src/main/java/de/hfu/MainB5.java
+++ src/main/java/de/hfu/MainB5.java
@@ -6,11 +6,19 @@ import java.io.IOException;
 
 public class MainB5 {
 
+	/**
+	 * Main function for reading console input and convert it to upper case
+	 * @param args
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter String\n");
+        
+		/*save console input in parameter consoleInput*/
+		System.out.print("Enter String\n");
         String consoleInput = br.readLine();
         
+        /*convert input to upper case*/
         System.out.print("Entered String in Uppercase\n");
         System.out.print(consoleInput.toUpperCase());
 
